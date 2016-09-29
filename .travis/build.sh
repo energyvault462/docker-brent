811a1ed17905638074e4a998fab6e2b09ac86a66
Daniel Hess <dan9186@gmail.com>
2016-09-28 17:21:29 -0700

    more changes for new command

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #GIT_CHANGES=$(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | cut -d/ -f1 | grep -v '.travs.yml')
-GIT_CHANGES=$(git diff --name-only HEAD $(git merge-base HEAD origin/master))
+GIT_CHANGES=$(git diff --name-only HEAD)
 
 if [[ "$GIT_CHANGES" == *"Dockerfile"* ]]; then
 	echo "Dockerfile changes detected, testing docker build"
