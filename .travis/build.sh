7e5a6eaff3825b6496a6d5aacd245a9ad3af437f
Daniel Hess <dan9186@gmail.com>
2016-09-28 17:08:22 -0700

    going back to travis script builds

---
+++ .travis/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#GIT_CHANGES=$(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | cut -d/ -f1 | grep -v '.travs.yml')
+GIT_CHANGES=$(git diff --name-only HEAD $(git merge-base HEAD master))
+
+if [[ "$GIT_CHANGES" == *"Dockerfile"* ]]; then
+	echo "Dockerfile changes detected, testing docker build"
+	docker build \
+		--build-arg VCS_REF=`git rev-parse --short HEAD` \
+		--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+		.
+fi
