855ae0138534378859e96436628f2d39a500dcf0
Daniel Hess <dan9186@gmail.com>
2016-09-28 18:21:30 -0700

    forgot to toss old build var

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,8 +9,9 @@ echo "Files Changes: $GIT_CHANGES"
 if [[ "$GIT_CHANGES" == *"Dockerfile"* ]]; then
 	echo "Dockerfile changes detected, testing docker build"
 	docker build \
-		--build-arg VCS_REF=`git rev-parse --short HEAD` \
 		--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+		--build-arg BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
+		--build-arg VERSION="0.0.1" \
 		.
 else
 	echo "No Dockerfile changes, skipping docker build"
