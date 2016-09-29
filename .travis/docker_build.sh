48e86fdde1612ac7eacc979df6d10bd6f940dee4
Daniel Hess <dan9186@gmail.com>
2016-09-28 23:03:51 -0700

    moving build to docker build and universalizing git changes

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GIT_CHANGES=$(git diff --name-only origin/master..HEAD)
-echo "Files Changes: $GIT_CHANGES"
-
 if [[ "$GIT_CHANGES" == *"Dockerfile"* ]]; then
 	echo "Dockerfile changes detected, testing docker build"
 	docker build \
