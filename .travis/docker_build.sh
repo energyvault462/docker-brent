1f8b490518fe86bbbc6bb271373a6f50e1c47a62
Daniel Hess <dan9186@gmail.com>
2016-10-01 18:58:07 -0700

    expecting strings from env

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-if [[ "$GIT_CHANGES" == *"Dockerfile"* || "$DUBUG" = true ]]; then
+if [[ "$GIT_CHANGES" == *"Dockerfile"* || "$DUBUG" == "true" ]]; then
 	echo "Dockerfile changes detected\n"
 
 	BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -21,6 +21,10 @@ if [[ "$GIT_CHANGES" == *"Dockerfile"* || "$DUBUG" = true ]]; then
 		--build-arg BUILD_NUMBER=$BUILD_NUMBER \
 		--build-arg VERSION=$IMAGE_VERSION \
 		.
+
+	echo "Testing Docker Image"
+	[ "$(docker run -it --rm $IMAGE_NAME whomai)" == "dan9186" ]
+
 else
 	echo "No Dockerfile changes, skipping docker build"
 fi
