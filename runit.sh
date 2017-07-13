5df9107b2fd9f5fbf38f7b259de2d18579f59882
brent johnson <brentj433@gmail.com>
2017-07-13 15:12:49 -0700

    updates runit script to change folders brought in

--- runit.sh
+++ runit.sh
@@ -4,7 +4,7 @@ docker run -it -v /home/brent/.gitconfig:/home/brent/.gitconfig \
 -v /home/brent/.ssh:/home/brent/.ssh  \
 -v /ram:/ram \
 -v /log:/log \
--v /home/brent/projects:/home/brent/projects \
+-v /nas/code:/home/brent/code \
 -v /nas:/nas energyvault462/brent 
 #-v $PWD:/docker \
 
